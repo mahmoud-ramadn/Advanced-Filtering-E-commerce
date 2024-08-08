7ec52d74c9c9efd3fb88934d2a7d72c7b36a62e7
mahmoud-ramadn <mr9026573@gmail.com>
2024-08-08 20:47:40 +0300

    first commit

---
+++ src/Components/Button.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const Button = ({ onClickHandler, value, title }) => {
+  return (
+    <button onClick={onClickHandler} value={value} className="btns">
+      {title}
+    </button>
+  );
+};
+
+export default Button;
